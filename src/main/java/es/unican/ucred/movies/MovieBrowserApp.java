767bbf26a2f731cb1f47ac75f1429cc4983c1eb4
Monica Puig-Pey <puigpeym@gestion.unican.es>
2026-03-11 10:08:58 +0100

    lo arreglo

--- src/main/java/es/unican/ucred/movies/MovieBrowserApp.java
+++ src/main/java/es/unican/ucred/movies/MovieBrowserApp.java
@@ -10,7 +10,7 @@ import java.awt.*;
 /**
  * Main application class for the UCRED Movies Browser.
  */
-public class MovieBrowserApp 
+public class MovieBrowserApp {
     public static void main(String[] args) {
         // Set system look and feel
         try {
